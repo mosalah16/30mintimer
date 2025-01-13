71a15c7b85cacb5dbc405c9115ece65cfcf35eb3
mosalah16 <allouchemohamedsalah1609@gmail.com>
2025-01-13 19:57:18 +0100

    reset button

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import pygame
 pygame.mixer.init()
 
 def start():
-    global t
-    global starting_time
+    global t,starting_time,remaining_time
     if starting_time==0:
         starting_time=int(time.monotonic())
     
@@ -22,7 +21,7 @@ def start():
         root.after(100,start)
     elif remaining_time==0:
         counter.config(text="00:00")
-        pygame.mixer.music.load("Kalimba.mp3")
+        pygame.mixer.music.load('assets/Kalimba.mp3')
         pygame.mixer.music.play()
         
 def change():
@@ -31,35 +30,45 @@ def change():
     x=int(x)*60
     if x!=0:
         t=x
+        mins=t//60
+        secs=t%60
+        ct=f'{mins:02}:{secs:02}'
+        counter.config(text=ct)
     elif x==0:
         t=1800
-        
-    
+
+def reset():
+    global remaining_time, starting_time
+    starting_time=0
+    remaining_time=t
+    mins=t//60
+    secs=t %60
+    ct=f'{mins:02}:{secs:02}'
+    counter.config(text=ct)
 
 
+remaining_time=0
 starting_time=0
 t=1800
 
 root = tk.Tk()
 root.title("Timer")
 
-txt=Label(root, text='By picking 0 you are setting it to a default 30 minutes')
-txt.grid(row=1)
-
 
 changet=IntVar()
-changet=Spinbox(root, from_=0, to=120)
+changet=Spinbox(root, from_=0, to=10000)
 changet.grid(row=0, column=0)
 
 
-
-counter=tk.Label(root, text="Pick your time in min and press start")
+counter=tk.Label(root, text="30:00")
 counter.grid(row=3)
 
 starting_button = tk.Button(root, text="start", command=start, width=5)
 starting_button.grid(row=2)
 change_button = tk.Button(root, text="change", command=change, width=5)
 change_button.grid(row=0, column=1)
+reset_button = tk.Button(root, text="reset",command=reset, width=5)
+reset_button.grid(row=2, column=1)
 
 root.mainloop()
 
