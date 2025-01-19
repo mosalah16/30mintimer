4aba2b2ddcfbbb109886cd262e9fd7babc7515c7
taharallouche <tahar.allouche.pro@gmail.com>
2025-01-19 20:07:13 +0100

    feat: format and rename

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
 import tkinter as tk
 
-#import pygame
+# import pygame
 
 TARGET_DURATION = 1800
+
 
 class Time:
     target_duration: int
@@ -13,28 +14,22 @@ class Time:
         self.target_duration = target_duration
         self.starting_time = None
 
-    def start(self) -> None:
+    def _set_starting_time(self) -> None:
         if self.starting_time is None:
             self.starting_time = int(time.monotonic())
-    
+
     def change(self, new_time: int) -> None:
-        self.target_duration=new_time
+        self.target_duration = new_time
 
     def reset(self) -> None:
         self.starting_time = None
 
     def _time_elapsed(self) -> int:
+        self._set_starting_time()
         return int(time.monotonic()) - self.starting_time
 
     def remaining_time(self) -> int:
-        self.start()
         return self.target_duration - self._time_elapsed()
-
-    @staticmethod
-    def format_remaining_time(remaining_time: int) -> None:
-        mins = remaining_time // 60
-        secs = remaining_time % 60
-        return f"{mins:02}:{secs:02}"
 
 
 class Counter:
@@ -45,42 +40,46 @@ class Counter:
 
     def reset(self) -> None:
         self.time.reset()
-    
+
     def change(self) -> None:
-        new_time=(int(change_time.get()))*60
-        if new_time==0:
-            new_time=1800
+        new_time = (int(change_time.get())) * 60
+        if new_time == 0:
+            new_time = 1800
         self.time.change(new_time)
         self.time.reset()
-        self.update(new_time)
-    
+        self.update_display_remaining_time(new_time)
+
     def run(self):
         remaining_time = self.time.remaining_time()
-        self.update(remaining_time)
+        self.update_display_remaining_time(remaining_time)
         if remaining_time == 0:
             self.stop()
         else:
             root.after(100, self.run)
 
-
     def stop(self) -> None:
         pass
-        #pygame.mixer.music.load("assets/Kalimba.mp3")
-        #pygame.mixer.music.play()
+        # pygame.mixer.music.load("assets/Kalimba.mp3")
+        # pygame.mixer.music.play()
 
-    def update(self, new_time: int) -> None:
-        new_time = self.time.format_remaining_time(new_time)
+    def update_display_remaining_time(self, new_time: int) -> None:
+        new_time = self.format_remaining_time(new_time)
         self.widget.config(text=new_time)
-        
+
+    @staticmethod
+    def format_remaining_time(remaining_time: int) -> None:
+        mins = remaining_time // 60
+        secs = remaining_time % 60
+        return f"{mins:02}:{secs:02}"
 
 
 if __name__ == "__main__":
-    #pygame.mixer.init()
+    # pygame.mixer.init()
 
     root = tk.Tk()
     root.title("Timer")
 
-    counter_widget = tk.Label(root, text=Time.format_remaining_time(TARGET_DURATION))
+    counter_widget = tk.Label(root, text=Counter.format_remaining_time(TARGET_DURATION))
     counter_widget.grid(row=1)
 
     counter = Counter(target_duration=TARGET_DURATION, widget=counter_widget)
@@ -94,9 +93,8 @@ if __name__ == "__main__":
     change_button = tk.Button(root, text="change", command=counter.change, width=5)
     change_button.grid(row=4)
 
-    change_time=tk.IntVar()
-    change_time=tk.Spinbox(root, from_=0, to=10000)
+    change_time = tk.IntVar()
+    change_time = tk.Spinbox(root, from_=0, to=10000)
     change_time.grid(row=0)
-
 
     root.mainloop()
